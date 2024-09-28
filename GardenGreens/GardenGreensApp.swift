1270a59a0013795bd2d9da3a4888d598f5e4e5c0
Renato Oliveira Fraga <rofraga74@gmail.com>
2024-09-27 20:03:17 -0500

    Initial Commit

---
+++ GardenGreens/GardenGreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenGreensApp.swift
+//  GardenGreens
+//
+//  Created by Renato Oliveira Fraga on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GardenGreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
